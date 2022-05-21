93a86957bc6dff66c94faa6a13e7cc917a571ee1
NhShanto01 <mdnurhossain940@gmail.com>
2022-05-22 01:43:58 +0600

    mandatory packages install and some components added

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#fde047",
+
+          "secondary": "#2563eb",
+
+          "accent": "#d97706",
+
+          "neutral": "#191D24",
+
+          "base-100": "#f3f4f6",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
